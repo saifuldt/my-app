69ef5cdf62d5935db56d44e01d94f7e7e4c7b619
saifuldt <saifulislam897722@gmail.com>
2024-09-14 20:49:28 +0600

    first commit

---
+++ src/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <div>
+        <button className='text-center border-y-4 border-slate-500 w-28 py-1 text-lg font-bold rounded-lg hover:text-gray-800' >Hire Me</button>
+
+    </div>
+  )
+}
+
+export default Button
