70f366d3d816f06bd63cf1d9bc1e453a9a757efe
saifuldt <saifulislam897722@gmail.com>
2024-09-16 20:57:07 +0600

    component update

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
-import React from 'react'
-import mypic from '/src/assets/mypic.jpg'
-import Button from './Button'
+
+import Button from './component/Button';
+import Card from './component/Card';
+import Footer from './component/Footer';
+import Navbar from './component/Navbar';
+
 function App() {
   return (
-    <div className="flex justify-center items-center w-[100%] h-[100vh] bg-slate-800">
-      <div className="flex justify-center items-center bg-slate-400 p-2 rounded-lg ">
-
-        <div className="flex flex-col justify-center items-center gap-1">
-          
-          <img className="w-[100px] h-[100px] rounded-full" src={mypic}/>
-          
-          <Button />
-        </div>
-        <div className="profile px-2 uppercase">
-          <h1 className='text-3xl font-bold'>saiful islam</h1>
-          <h2 className='font-semibold '>frontent developer</h2>
-          <p className='lowercase pb-2  '>saifulislam897722@gmail.com </p>
-
-        </div>
+    <>
+      <Navbar />
+      <Button />
+      <div className="flex justify-around">
+        <Card />
+        <Card />
+        <Card />
       </div>
-    </div>
+      <div className="flex justify-around">
+        <Card />
+        <Card />
+        <Card />
+      </div>
+      <div className="flex justify-around">
+        <Card />
+        <Card />
+        <Card />
+      </div>
+      <Footer/>
+
+    </>
   )
 }
 
---
+++ src/component/Button.jsx
@@ -0,0 +1,45 @@
+
+const Button = () => {
+  return (
+    <div className="carousel w-full">
+  <div id="slide1" className="carousel-item relative w-full">
+    <img
+      src="https://www.rei.com/dam/up_2018-02-c01_2972_running_shoes_fit.jpg?t=ea16by9lg"
+      className="w-full h-96" />
+    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+      <a href="#slide4" className="btn btn-circle">❮</a>
+      <a href="#slide2" className="btn btn-circle">❯</a>
+    </div>
+  </div>
+  <div id="slide2" className="carousel-item relative w-full">
+    <img
+      src="https://runkeeper.com/cms/wp-content/uploads/sites/4/2021/12/ASICS_Color-Injection-Pack_Highlight_0253-scaled.jpg"
+      className="w-full h-96" />
+    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+      <a href="#slide1" className="btn btn-circle">❮</a>
+      <a href="#slide3" className="btn btn-circle">❯</a>
+    </div>
+  </div>
+  <div id="slide3" className="carousel-item relative w-full">
+    <img
+      src="https://media.gq.com/photos/63eba1b2275d2fef78a425c2/16:9/w_1600,c_limit/nike-running-shoes-streakfly-invincible.jpg"
+      className="w-full h-96" />
+    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+      <a href="#slide2" className="btn btn-circle">❮</a>
+      <a href="#slide4" className="btn btn-circle">❯</a>
+    </div>
+  </div>
+  <div id="slide4" className="carousel-item relative w-full">
+    <img
+      src="https://media.cnn.com/api/v1/images/stellar/prod/comfy-shoes-nike-new-lead.jpg?c=16x9&q=h_653,w_1160,c_fill/f_webp"
+      className="w-full h-96" />
+    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+      <a href="#slide3" className="btn btn-circle">❮</a>
+      <a href="#slide1" className="btn btn-circle">❯</a>
+    </div>
+  </div>
+</div>
+  )
+}
+
+export default Button
